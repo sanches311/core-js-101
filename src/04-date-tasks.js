6c15237d7e013ae550a10488b4f689b23bb0937b
sanches311 <itchipset@gmail.com>
2022-12-02 14:06:28 +0300

    feat: add 04-date-tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,12 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  if (year % 4 !== 0) return false;
+  if (year % 100 !== 0) return true;
+  if (year % 400 !== 0) return false;
+  return true;
 }
 
 
@@ -73,10 +77,27 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const hours = endDate.getHours() - startDate.getHours();
+  const minutes = endDate.getMinutes() - startDate.getMinutes();
+  const seconds = endDate.getSeconds() - startDate.getSeconds();
+  const milliSeconds = endDate.getMilliseconds() - startDate.getMilliseconds();
+  let formatHours = '';
+  let formatMinutes = '';
+  let formatSeconds = '';
+  let formatMilliseconds = '';
+  if (hours < 9) formatHours += `0${hours.toString()}`;
+  else { formatHours += `${hours.toString()}`; }
+  if (minutes < 9) formatMinutes += `0${minutes.toString()}`;
+  else { formatMinutes += `${minutes.toString()}`; }
+  if (seconds < 9) formatSeconds += `0${seconds.toString()}`;
+  else { formatSeconds += `${seconds.toString()}`; }
+  if ((milliSeconds > 9) && (milliSeconds < 99)) { formatMilliseconds += `0${milliSeconds.toString()}`; }
+  if (milliSeconds < 9) formatMilliseconds += `00${milliSeconds.toString()}`;
+  else { formatMilliseconds += `${milliSeconds.toString()}`; }
+  return `${formatHours}:${formatMinutes}:${formatSeconds}.${formatMilliseconds}`;
 }
-
+// console.log(timeSpanToString(new Date(2000, 1, 1, 10, 0, 0), new Date(2000, 1, 1, 11, 0, 0)));
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock
